7286d577f2bcf7caa1892e51177ce6a1823f9ef8
GabrielMRodrigues <gabrielm1212@gmail.com>
2023-04-06 01:03:09 -0300

    Finalizado

---
+++ src/Gobi.Test.Jr.Domain/Interfaces/ITodoItemRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Gobi.Test.Jr.Domain.Interfaces
+{
+    public interface ITodoItemRepository
+    {
+        IEnumerable<TodoItem> GetAll();
+        void Add(TodoItem todoItem);
+        void Update(TodoItem todoItem);
+        TodoItem GetById(int todoItemId);
+        void Delete(int todoItemId);
+    }
+}
---
+++ src/Gobi.Tests.Jr.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Gobi.Test.Jr.Application;
+using Gobi.Test.Jr.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gobi.Tests.Jr.WebApp.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly TodoItemService _todoItemService;
+
+        public TodoController(TodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+
+        public IActionResult Index()
+        {
+            var items = _todoItemService.GetAll();
+
+            return View(items);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View(new TodoItem());
+        }
+
+        [HttpPost]
+        public IActionResult Add(TodoItem todoItem)
+        {
+            _todoItemService.Add(todoItem);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int todoItemId)
+        {
+            TodoItem todoItem = _todoItemService.GetById(todoItemId);
+            return View(todoItem);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TodoItem todoItem)
+        {
+            _todoItemService.Update(todoItem);
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Delete(int todoItemId)
+        {
+            _todoItemService.Delete(todoItemId);
+            return RedirectToAction("Index");
+        }
+    }
+}
